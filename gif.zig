00756d26b082feb2dba5905c3ff72f28404a1d1a
Mick Sayson <mick@sayson.com>
2026-02-16 15:56:15 -0800

    holy  crap

--- gif.zig
+++ gif.zig
@@ -211,50 +211,40 @@ const LzwDecompressor = struct {
 
     alloc: std.mem.Allocator,
 
-    dictionary: std.ArrayListUnmanaged(Sequence),
-    reverse_dictionary: ReverseDict,
+    dict2: Dict,
 
     last_code: ?Code = null,
 
-    const ReverseDict = std.HashMapUnmanaged(Sequence, Code, SeqContext, std.hash_map.default_max_load_percentage);
-    pub const SeqContext = struct {
-        pub fn hash(self: @This(), s: []const Code) u64 {
-            _ = self;
-            var hasher = std.hash.Wyhash.init(0);
-            std.hash.autoHashStrat(&hasher, s, .DeepRecursive);
-            return hasher.final();
-        }
+    buffered_seq_2: std.ArrayList(Code),
 
-        pub fn eql(self: @This(), a: []const Code, b: []const Code) bool {
-            _ = self;
-            return std.meta.eql(a, b);
-        }
-    };
     const Code = u12;
     const Sequence = []u12;
 
 
     pub fn init(alloc: std.mem.Allocator, initial_code_len: u8, r: *std.Io.Reader) !LzwDecompressor {
-        var dict: std.ArrayListUnmanaged(Sequence) = .{};
-        var reverse_dict: ReverseDict = .{};
-
-        try rebuildDict(alloc, initial_code_len, &dict, &reverse_dict);
-
         return .{
             .input = r,
             .initial_code_size = initial_code_len,
             .code_size_bits = initial_code_len + 1,
             .next_byte = 0,
             .bits_remaining = 0,
+            .buffered_seq_2 = .initBuffer(try alloc.alloc(Code, 128)),
             .alloc = alloc,
-            .dictionary = dict,
-            .reverse_dictionary = reverse_dict,
             .last_code = null,
+            .dict2 = .{
+                .alloc = alloc,
+                .initial_len = initial_code_len,
+                .inner = .{},
+            },
         };
 
     }
 
-    pub fn step(self: *LzwDecompressor) !?Sequence {
+    pub fn step(self: *LzwDecompressor) !?u12 {
+        if (self.buffered_seq_2.pop()) |val| {
+            return val;
+        }
+
         //std.debug.print("next code\n", .{});
         const next_code = try self.readCode();
 
@@ -263,10 +253,11 @@ const LzwDecompressor = struct {
         if (next_code == @as(u12, 1) << @intCast(self.initial_code_size)) {
             std.debug.print("Rebuilding dictionary: {d}\n", .{self.initial_code_size});
             self.code_size_bits = self.initial_code_size;
-            try rebuildDict(self.alloc, self.initial_code_size, &self.dictionary, &self.reverse_dictionary);
+            self.dict2.reset();
             self.last_code = null;
             self.code_size_bits = self.initial_code_size + 1;
-            return &.{};
+            // FIXME: recursion
+            return self.step();
         }
 
         if (next_code == (@as(u12, 1) << @intCast(self.initial_code_size)) + 1) {
@@ -276,58 +267,72 @@ const LzwDecompressor = struct {
 
         defer self.last_code = next_code;
 
-        //std.debug.print("lzw size: {d}, dict_size: {d}\n", .{self.code_size_bits, self.dictionary.items.len});
 
-        if (next_code >= self.dictionary.items.len) {
+        if (!self.dict2.inRagne(next_code)) {
 
-            const last_sequence = self.dictionary.items[self.last_code.?];
-
-            const combined = try self.alloc.alloc(Code, last_sequence.len + 1);
-            @memcpy(combined[0..last_sequence.len], last_sequence);
-            combined[last_sequence.len] = last_sequence[0];
-
-            if (self.dictionary.items.len + 1 >= (@as(Code, 1) << @intCast(self.code_size_bits))) {
-                self.code_size_bits += 1;
+            var last_seq_start: Code = undefined;
+            {
+                var it = self.dict2.parentIter(self.last_code.?);
+                while (it.next()) |val| last_seq_start = val;
             }
 
-            try self.dictionary.append(self.alloc, combined);
-            std.debug.print("Inserted {any} at {d}\n", .{combined, self.dictionary.items.len - 1});
+            std.debug.print("path 1\n", .{});
 
-            return combined;
+            const new_code =try self.dict2.append(last_seq_start, self.last_code.?);
+            self.updateCodeSize(new_code);
+            var it = self.dict2.parentIter(new_code);
+            self.buffered_seq_2.clearRetainingCapacity();
+
+            while (it.next()) |code| {
+                std.debug.print("got elem {d}\n", .{code});
+                try self.buffered_seq_2.appendBounded(code);
+            }
+
+            return self.buffered_seq_2.pop().?;
         }
 
-        const next_sequence = self.dictionary.items[next_code];
+        var it = self.dict2.parentIter(next_code);
+        self.buffered_seq_2.clearRetainingCapacity();
+
+
+        while (it.next()) |code| {
+            std.debug.print("got elem {d}\n", .{code});
+            try self.buffered_seq_2.appendBounded(code);
+        }
+
+        const ret = self.buffered_seq_2.pop().?;
+
         // FIXME: Actually error instead of crash
         if (self.last_code) |lc| {
             std.debug.print("last code: {d}\n", .{lc});
-            const last_sequence = self.dictionary.items[lc];
+            std.debug.print("path 2\n", .{});
 
-            const combined = try self.alloc.alloc(Code, last_sequence.len + 1);
-            @memcpy(combined[0..last_sequence.len], last_sequence);
-            combined[last_sequence.len] = next_sequence[0];
+            self.updateCodeSize(try self.dict2.append(ret, lc));
 
-
-            if (self.dictionary.items.len + 1 >= (@as(Code, 1) << @intCast(self.code_size_bits))) {
-                self.code_size_bits += 1;
-            }
-
-            try self.dictionary.append(self.alloc, combined);
-            std.debug.print("Inserted {any} at {d}\n", .{combined, self.dictionary.items.len - 1});
         }
+        return ret;
 
-        return next_sequence;
     }
 
-    fn rebuildDict(alloc: std.mem.Allocator, initial_code_len: u8, dict: *std.ArrayListUnmanaged(Sequence), reverse_dict: *ReverseDict) !void {
+    fn updateCodeSize(self: *LzwDecompressor, new_code: Code) void {
+        if (new_code + 1 >= (@as(Code, 1) << @intCast(self.code_size_bits))) {
+            self.code_size_bits += 1;
+        }
+    }
+
+    fn appendSequence(self: *LzwDecompressor, seq: []u12) !void {
+        try self.dictionary.append(self.alloc, seq);
+        std.debug.print("Inserted {any} at {d}\n", .{seq, self.dictionary.items.len - 1});
+    }
+
+    fn rebuildDict(alloc: std.mem.Allocator, initial_code_len: u8, dict: *std.ArrayListUnmanaged(Sequence)) !void {
         dict.clearRetainingCapacity();
-        reverse_dict.clearRetainingCapacity();
 
         // FIXME: dict doesn't need 0..initial_code_len idiot
         for (0..(@as(usize, 1) << @intCast(initial_code_len))) |i| {
             const sequence = try alloc.alloc(Code, 1);
             sequence[0] = @intCast(i);
             try dict.append(alloc, sequence);
-            try reverse_dict.put(alloc, sequence, @intCast(i));
         }
 
         try dict.append(alloc, &.{});
@@ -370,6 +375,128 @@ const LzwDecompressor = struct {
         return out;
     }
 
+    const Dict = struct {
+        alloc: std.mem.Allocator,
+        inner: std.ArrayList(DictItem),
+        initial_len: u8,
+
+        // Index (where in inner we live)
+        // Code (value coming from decoder)
+        // Sequence item (element to be exposed to caller)
+
+        const Idx = struct {
+            inner: usize,
+        };
+
+        const SequenceItem = struct {
+            inner: Code,
+        };
+
+        const DictItem = struct {
+            parent: Code,
+            item: SequenceItem,
+        };
+
+        const QueryRes = struct {
+            parent: ?Code,
+            item: SequenceItem,
+        };
+
+
+        fn query(self: *Dict, code: Code) QueryRes {
+            const offs = self.calcOffs();
+            if (code < offs) {
+                return .{
+                    .parent = null,
+                    .item = .{ .inner = code },
+                };
+            }
+
+            const item = self.inner.items[code - offs];
+            return .{
+                .parent = item.parent,
+                .item = item.item,
+            };
+        }
+
+        fn inRagne(self: *Dict, item: Code) bool {
+            const offs = self.calcOffs();
+            if (item < offs) return true;
+            return item - offs < self.inner.items.len;
+        }
+
+        fn append(self: *Dict, item: Code, parent: Code) !Code {
+            std.debug.assert(item < self.calcOffs());
+            const idx = self.inner.items.len;
+            std.debug.print("Appending: {d} at {d}\n", .{item, self.calcOffs() + idx});
+            try self.inner.append(self.alloc, .{
+                .item = .{ .inner = item },
+                .parent = parent,
+            });
+            return @intCast(self.calcOffs() + idx);
+        }
+
+        fn getIdx(self: *Dict, idx: Idx) DictItem {
+            return self.inner.items[idx.inner];
+        }
+
+        fn codeToIdx(self: *Dict, code: Code) ?Idx {
+            const offs = self.calcOffs();
+            if (code < offs) {
+                return null;
+            }
+            return .{ .inner = code - offs };
+        }
+
+        const ParentIter = struct {
+            current: ?Code,
+            dict: *Dict,
+
+            pub fn next(self: *ParentIter) ?Code {
+                const current = self.current orelse return null;
+
+                const query_res = self.dict.query(current);
+                self.current = query_res.parent;
+                return query_res.item.inner;
+            }
+        };
+
+        fn parentIter(self: *Dict, item: Code) ParentIter {
+            return .{
+                .current = item,
+                .dict = self,
+            };
+        }
+
+        fn calcOffs(self: *Dict) u12 {
+            return (@as(u12, 1) << @as(u4, @intCast(self.initial_len))) + 2;
+        }
+
+        fn codeAsSeqItem(self: *Dict, item: Code) ?SequenceItem {
+            const offs = self.calcOffs();
+            if (item < offs) {
+                return .{ .inner = item };
+            }
+
+            const idx = item - offs;
+            return self.inner.items[idx].item;
+        }
+
+        fn getParent(self: *Dict, item: Code) ?Code {
+            const offs = self.calcOffs();
+            if (item < offs) {
+                return null;
+            }
+
+            const idx = item - offs;
+            return self.inner.items[idx].parent;
+        }
+
+        fn reset(self: *Dict) void {
+            self.inner.clearRetainingCapacity();
+        }
+    };
+
 };
 
 //test "LzwDecompressor readCode" {
@@ -393,30 +520,21 @@ test "4 byte lzw decomrpession" {
     var alloc_buf: [1 * 1024 * 1024]u8 = undefined;
     var alloc = std.heap.FixedBufferAllocator.init(&alloc_buf);
 
-    var scratch_buf: [1 * 1024 * 1024]u8 = undefined;
-    var scratch = std.heap.FixedBufferAllocator.init(&scratch_buf);
-
     const input = &.{0x5c, 0x04, 0x05};
     var r = std.Io.Reader.fixed(input);
     var lzwd = try LzwDecompressor.init(alloc.allocator(), 2, &r);
 
     const expected: []const u12 = &.{3, 1, 2, 0};
     var i: usize = 0;
-    while (try lzwd.step()) |seq| {
-        scratch.end_index = 0;
-        for (seq) |elem| {
-            try std.testing.expectEqual(expected[i], elem);
-            i += 1;
-        }
+    while (try lzwd.step()) |elem| {
+        try std.testing.expectEqual(expected[i], elem);
+        i += 1;
     }
 }
 
 test "36 byte lzw decomrpession" {
     var alloc_buf: [1 * 1024 * 1024]u8 = undefined;
     var alloc = std.heap.FixedBufferAllocator.init(&alloc_buf);
-
-    var scratch_buf: [1 * 1024 * 1024]u8 = undefined;
-    var scratch = std.heap.FixedBufferAllocator.init(&scratch_buf);
 
     const input = &.{0x44, 0x6c, 0xa7, 0x80,  0xba, 0xd7, 0x52, 0x2c};
     var r = std.Io.Reader.fixed(input);
@@ -432,11 +550,8 @@ test "36 byte lzw decomrpession" {
     };
 
     var i: usize = 0;
-    while (try lzwd.step()) |seq| {
-        scratch.end_index = 0;
-        for (seq) |elem| {
-            try std.testing.expectEqual(expected[i], elem);
-            i += 1;
-        }
+    while (try lzwd.step()) |elem| {
+        try std.testing.expectEqual(expected[i], elem);
+        i += 1;
     }
 }
